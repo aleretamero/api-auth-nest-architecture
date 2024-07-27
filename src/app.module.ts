5264dfa158ebd1b94e875d3eecceed4ea394710b
Alexandre Retamero <alexandreretamero@gmail.com>
2024-07-27 11:57:52 -0300

    refactor: Remove commented out code and unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '@/common/guards/auth.guard';
+// import { APP_GUARD } from '@nestjs/core';
+// import { AuthGuard } from '@/common/guards/auth.guard';
 
-import ModulesModules from '@/modules';
-import InfraModules from '@/infra';
-import { ThrottlerGuard } from '@nestjs/throttler';
+// import ModulesModules from '@/modules';
+// import InfraModules from '@/infra';
+// import { ThrottlerGuard } from '@nestjs/throttler';
 import { AppController } from '@/app.controller';
 
+// @Module({
+//   imports: [...InfraModules, ...ModulesModules],
+//   controllers: [AppController],
+//   providers: [
+//     { provide: APP_GUARD, useClass: ThrottlerGuard }, // call ThrottlerGuard first so it runs before
+//     { provide: APP_GUARD, useClass: AuthGuard },
+//   ],
+// })
 @Module({
-  imports: [...InfraModules, ...ModulesModules],
   controllers: [AppController],
-  providers: [
-    { provide: APP_GUARD, useClass: ThrottlerGuard }, // call ThrottlerGuard first so it runs before
-    { provide: APP_GUARD, useClass: AuthGuard },
-  ],
 })
 export class AppModule {}
